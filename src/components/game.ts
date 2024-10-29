2c10e42cf7cb5cd8e4affee8f0d585dfb2eb2f7b
mcorreira <megancorreiradev@gmail.com>
2024-10-29 07:26:24 -0400

    collision and food

---
+++ src/components/game.ts
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function game(state): JSX.Element {
+  return <SafeAreaView>{/* Game component */}</SafeAreaView>;
+}
